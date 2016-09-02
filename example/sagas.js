52ad2c0d8b24fc57d29efbced07e04370f5e5ae0
John Crygier <john@crygier.com>
2016-09-01 21:31:42 -0500

    Blank out p/w after success login. Null out inactivity watcher, so it works for round #2+

--- example/sagas.js
+++ example/sagas.js
@@ -3,13 +3,20 @@
  */
 
 import { push } from 'react-router-redux';
-import { take, put } from 'redux-saga/effects';
+import { take, put, race } from 'redux-saga/effects';
 
-import { AUTH_SUCCESS } from '../lib/constants';
+import { AUTH_SUCCESS, SIGN_OUT } from '../lib/constants';
 
-export function* listenForAuthSuccess() : Generator<*, *, *> {
+export function* listenForAuthChange() : Generator<*, *, *> {
   while (true) {  // eslint-disable-line
-    const authSuccess = yield take(AUTH_SUCCESS);
-    yield put(push('/'));     // TODO: check for redirection parameter?
+    const raceResponse = yield race({
+      signOut: take(SIGN_OUT),
+      authSuccess: take(AUTH_SUCCESS),
+    });
+
+    if (raceResponse && raceResponse.authSuccess)
+      yield put(push('/'));     // TODO: check for redirection parameter?
+    else
+      yield put(push('/login'));     // TODO: put in redirection parameter?
   }
 }
--- example/store.js
+++ example/store.js
@@ -9,7 +9,7 @@ import createSagaMiddleware from 'redux-saga';
 import createReducer from './reducers';
 import SagaMonitor from './utils/sagaMonitor';
 import { authenticationSaga } from '../lib/sagas';
-import { listenForAuthSuccess } from './sagas';
+import { listenForAuthChange } from './sagas';
 
 const sagaMiddleware = createSagaMiddleware({ sagaMonitor: SagaMonitor });
 const devtools = window.devToolsExtension || (() => noop => noop);
@@ -39,7 +39,7 @@ export default function configureStore(initialState = {}, history) {
 
   // Global Saga - Authenitcation
   sagaMiddleware.run(authenticationSaga());
-  sagaMiddleware.run(listenForAuthSuccess);
+  sagaMiddleware.run(listenForAuthChange);
 
   // Make reducers hot reloadable, see http://mxs.is/googmo
   /* istanbul ignore next */
